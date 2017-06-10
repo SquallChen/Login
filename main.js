8afca5441577eaed2703ab22aeaf147f27c19010
Dylan <442947365@qq.com>
2017-06-10 19:01:40 +0800

    重新调整一下翻页css

--- main.js
+++ main.js
@@ -65,16 +65,92 @@ var APP_ID = 'd1l7dVxFlCO3k9LxT9RhPriS-gzGzoHsz';
     })
 
 
+   let $contain =document.querySelector('#contain')
+   let $signUp = document.querySelector('.sign-up')
+   let $signIn = document.querySelector('.sign-in')
+   /*var self = $("#contain");
 
+ function rotateLeft(el) {
+            el.addClass("animation-rotate-left");
+        }
+        
+        function rotateLeftBack(el) {
+            el.addClass("animation-rotate-left-back");
+        }
+        
+        function rotateRight(el) {
+            el.addClass("animation-rotate-right");
+        }
+        
+        function rotateRightBack(el) {
+            el.addClass("animation-rotate-right-back");
+        }
+        
+        function loadPrev(){
+            rotateRightBack(self);
+            signUpForm.removeAttribute('hidden');
+            signInForm.setAttribute('hidden','hidden')
+        }
+        
+        function loadNext(){
+            rotateLeftBack(self);
+            signInForm.removeAttribute('hidden');
+            signUpForm.setAttribute('hidden','hidden');
+        }
 
-    let $signUp = document.querySelector('.sign-up')
-    $signUp.onclick = ()=>{
+        function rmright(){
+            $('contain').removeClass('animation-rotate-right animation-rotate-right-back');
+        }
+        function rmleft(){
+             $('contain').removeClass('animation-rotate-left animation-rotate-left-back')
+        }
+
+*/
+    function rmup(){
        signUpForm.removeAttribute('hidden');
        signInForm.setAttribute('hidden','hidden');
     }
-    
-     let $signIn = document.querySelector('.sign-in')
-    $signIn.onclick = ()=>{
+ 
+    function rmin(){
        signInForm.removeAttribute('hidden');
        signUpForm.setAttribute('hidden','hidden');
     }
+    function rmupA(){
+         $('#contain').removeClass('transgo');
+    }
+    function rminA(){
+         $('#contain').removeClass('transback');
+    }
+
+    function resetF1(){
+     signUpForm.style.transform='rotateY(0deg)';
+    }
+    function resetF2(){
+     signInForm.style.transform='rotateY(0deg)';
+    }
+
+
+    $signUp.onclick = ()=>{
+       /*signUpForm.removeAttribute('hidden');
+       signInForm.setAttribute('hidden','hidden');*/
+      /* rotateRight(self);
+       setTimeout(loadPrev, 500);
+       setTimeout(rmright, 1000);*/
+       $('#contain').addClass('transgo')
+       setTimeout(rmup, 500);
+       setTimeout(rmupA, 1000);
+    }
+    
+     
+    $signIn.onclick = ()=>{
+      /* signInForm.removeAttribute('hidden');
+       signUpForm.setAttribute('hidden','hidden');*/
+        /* rotateLeft(self);
+       setTimeout(loadNext, 500);
+        setTimeout(rmleft, 1000);*/
+     $('#contain').addClass('transback')
+     setTimeout(rmin, 500);
+     setTimeout(rminA, 1000);
+    }
+
+
